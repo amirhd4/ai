5c01130c3d9c41992cfa139a17c65e086a2bca17
amir0hd4@gmail.com <amir0hd4@gmail.com>
2025-01-11 11:18:13 +0330

    oil price ai

--- linear_regression/oil_price.py
+++ linear_regression/oil_price.py
@@ -2,14 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 df = pd.read_csv('files/edible_oil.csv')
 cdf = df[["YEAR", "PRICE"]]
 viz = cdf[["YEAR", "PRICE"]]
 
-msk = np.random.rand(len(df)) < 0.70
-train = cdf[msk]
-test = cdf[~msk]
+# msk = np.random.rand(len(df)) < 0.80
+# train = cdf[msk]
+# test = cdf[~msk]
+msk = np.random.rand(len(df)) < 1
+train, test = cdf[msk], cdf[msk]
 
 fig = plt.figure()
 pic1 = fig.add_subplot(111)
@@ -31,3 +34,7 @@ test_x = np.asanyarray(test[["YEAR"]])
 test_y = np.asanyarray(test[["PRICE"]])
 test_y_ = regr.predict(test_x)
 
+ys = np.array([[1404], [1405]])
+res = regr.predict(np.asanyarray(ys))
+print(res)
+# print("score: ", r2_score(test_y, test_y_))
